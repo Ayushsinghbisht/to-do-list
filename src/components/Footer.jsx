7e58e5884172ff01b31259b0ed1df6d8f39fc848
Ayush <ayushbisht750083@gmail.com>
2023-07-13 17:53:53 +0530

    message is adding to do list

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p className="footername">Ayush bisht </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>To do list by Ayush</h1>
+    </header>
+  );
+}
+
+export default Header;
